ce10f4a87e035d39ff57520742ae75ef2aeb0dbd
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-10-17 14:24:45 -0300

    chore: configuração e teste para simulação com Wokwi

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,35 @@
 #include <Arduino.h>
+
+const int potPin = 34; // Pin potensiometer
+const int ledPin = 4;  // Pin LED
+
+int potValue = 0;
+int ledValue = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(potPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    Serial.println("Monitoring potensiometer dan LED siap!");
+}
+
+void loop()
+{
+    // Baca nilai potensiometer (0–4095)
+    potValue = analogRead(potPin);
+
+    // Ubah ke skala 0–255 untuk kecerahan LED
+    ledValue = map(potValue, 0, 4095, 0, 255);
+
+    // Atur kecerahan LED langsung dengan analogWrite
+    analogWrite(ledPin, ledValue);
+
+    // Cetak ke Serial Monitor
+    Serial.print("Nilai Pot: ");
+    Serial.print(potValue);
+    Serial.print(" | Kecerahan LED: ");
+    Serial.println(ledValue);
+
+    delay(100);
+}
